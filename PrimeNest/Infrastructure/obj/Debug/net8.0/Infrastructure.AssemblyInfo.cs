844f236493e73a50a6f9aca977c607c351ae6f4c
redasaad <redasaad123@gmail.com>
2026-04-18 13:12:36 +0200

    commit

--- PrimeNest/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ PrimeNest/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4892301666025ba495ed074d11a3b4300d5776f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c9f5d8eae915f33c050cecd34feabca60514127")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
